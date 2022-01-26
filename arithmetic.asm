b2681e0dd67f2fe017e887789d27322209c23243
tigeryu8900 <tigeryu8900@gmail.com>
2022-01-25 21:40:48 -0500

    TASK1 : Arithmetic Exercise (20 marks)

--- arithmetic.asm
+++ arithmetic.asm
@@ -4,8 +4,25 @@
 
 .text
 main:
-
-	# TODO: Write your code here
+	# set $a0 as x
+	li $v0, 5
+	syscall
+	move $a0, $v0
+	
+	# subtract y from $a0
+	li $v0, 5
+	syscall
+	sub $a0, $a0, $v0
+	
+	# shift $a0 left by 6 (multiply by 64)
+	sll $a0, $a0, 6
+	
+	# or $a0 by 32 (same as add since the 5th bit of $a0 is 0)
+	ori $a0, $a0, 32
+	
+	li $v0, 1
+	syscall
 
 exit:
-	# Exit SPIM: Write your code here!
+	li $v0, 10
+	syscall
