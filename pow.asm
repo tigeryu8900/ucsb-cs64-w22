8c50bb374702723fcde3a86b6ac84021042105ac
tigeryu8900 <tigeryu8900@gmail.com>
2022-02-01 13:36:38 -0800

    Task 2: Basic Loops: Pow.asm

--- pow.asm
+++ pow.asm
@@ -15,14 +15,57 @@
 # Assembly (NON-RECURSIVE) code version of pow(x,n):
 #
 .data
-
-    # TODO: Write your initializations here
+    number:   .asciiz "Enter a number x:\n"
+    exponent: .asciiz "Enter the exponent n:\n"
+    answer:   .asciiz "Power(x,n) is:\n"
+    newline:  .asciiz "\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
+    # print "Enter a number x:\n"
+    li $v0, 4
+    la $a0, number
+    syscall
 
-    # TODO: Write your code here
+    # get integer
+    li $v0, 5
+    syscall
+    move $s0, $v0
+
+    # print "Enter the exponent n:\n"
+    li $v0, 4
+    la $a0, exponent
+    syscall
+
+    # get integer
+    li $v0, 5
+    syscall
+    move $s1, $v0
+
+    li $s2, 1
+
+    loop:
+        beqz $s1, then
+        mult $s0, $s2
+        mflo $s2
+        addi $s1, $s1, -1
+        j loop
+    then:
+
+    # print result
+    li $v0, 4
+    la $a0, answer
+    syscall
+    move $a0, $s2
+    li $v0, 1
+    syscall
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    j exit
 
 exit:
-    # TODO: Write code to properly exit a SPIM simulation
+    li  $v0, 10
+    syscall
