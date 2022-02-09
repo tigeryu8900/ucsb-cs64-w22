39c3e6d20ac054eb423242d1ddab048a699cc4bf
tigeryu8900 <tigeryu8900@gmail.com>
2022-02-08 16:43:31 -0800

    Task 1: Conversion Program

--- conversion.asm
+++ conversion.asm
@@ -4,8 +4,24 @@
 #   make all returned values from functions go in $v0
 
 .text
+j main
+
 conv:
-    # TODO: Write your function code here
+    li $v0, 0
+    li $t0, 0
+    loop:
+        sll $t1, $a0, 3
+        sub $v0, $v0, $t1
+        add $v0, $v0, $a1
+        slti $t1, $a0, 2
+        bnez $t1, end_if
+            addi $a1, $a1, -1
+        end_if:
+        addi $a0, $a0, 1
+        addi $t0, $t0, 1
+        slti $t1, $t0, 8
+        bnez $t1, loop
+    jr $ra
 
 main:
     li $a0, 5
@@ -18,4 +34,5 @@ main:
     syscall
 
 exit:
-    # TODO: Write code to properly exit a SPIM simulation
+    li $v0, 10
+    syscall
